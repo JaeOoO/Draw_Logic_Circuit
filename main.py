ed0f0d50afbaf840eaa9a86bed400803fdaa170e
JaeOoO <146085970+JaeOoO@users.noreply.github.com>
2023-09-27 12:40:11 +0000

    Change Layout

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ st.text('')
 expression = st.text_input('Write Your Logic Expression')
 if st.button('Submit'):
 
-    st.write(expression)
+    st.text('')
 
     with logicparse(expression) as drw:
         drw.save('temp.svg', transparent=False)
         image_bytes = drw.get_imagedata('svg')
 
     st.image('temp.svg')
-
-    st.download_button(label='Download data as SVG', data=image_bytes, file_name='MyCircuit.svg')
+    st.download_button(label='Download Image as SVG', data=image_bytes, file_name='MyCircuit.svg')
